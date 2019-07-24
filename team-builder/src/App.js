7f45318a03b9937c535a1df7f1cd39bf63544a82
deejayeaster <deejayeaster@live.com>
2019-07-24 17:36:52 -0400

    created app and forms

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,9 +1,15 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState } from "react";
+import Form from "./components/Form";
 import "./App.css";
 
 function App() {
-  return <div className="App" />;
+  const [user, setUser] = useState(""); // will add key value pairs
+
+  return (
+    <div className="App">
+      <Form />
+    </div>
+  );
 }
 
 export default App;
